615c00ae86e40aae7f4dbcf64cf2ac3aacb7113b
SHEKHAR GUPTA <137384631+shekhar-sg@users.noreply.github.com>
2025-05-22 14:13:36 +0530

    refactor ChatSection component layout and clean up unused imports

--- src/components/ChatSection.tsx
+++ src/components/ChatSection.tsx
@@ -4,7 +4,6 @@ import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Main from "@/material-components/Main";
 import ChatToolbar from "@/components/chat-toolbar";
-import Toolbar from "@mui/material/Toolbar";
 import UserList, { User } from "@/components/UserList";
 import DrawerComponent from "@/material-components/Drawer";
 import { Divider } from "@mui/material";
@@ -30,7 +29,7 @@ export default function ChatSection() {
   };
 
   // Handler for closing the chat (deselect user)
-  const handleCloseChat = () => setSelectedUserId(null);
+  // const handleCloseChat = () => setSelectedUserId(null);
   const selectedUser = users.find((u) => u.id === selectedUserId);
 
   return (
@@ -41,22 +40,22 @@ export default function ChatSection() {
         selectedUserId={selectedUserId}
         onSelectUser={setSelectedUserId}
       />
-      <Divider orientation={"vertical"}/>
+      <Divider orientation={"vertical"} />
       {/* Chat Section */}
       <Box sx={{ flex: 1, display: "flex", flexDirection: "column" }}>
         {/*<Navbar open={open} handleDrawerOpen={handleDrawerOpen} />*/}
-          <ChatToolbar
-            open={open}
-            handleDrawerOpen={handleDrawerOpen}
-            variant={"elevation"}
-            position={"sticky"}
-            sx={{
-              boxShadow: open ? "none" : "none",
-              bgcolor: "transparent",
-              color: "black",
-            }}
-          />
-        <Divider/>
+        <ChatToolbar
+          open={open}
+          handleDrawerOpen={handleDrawerOpen}
+          variant={"elevation"}
+          position={"sticky"}
+          sx={{
+            boxShadow: open ? "none" : "none",
+            bgcolor: "transparent",
+            color: "black",
+          }}
+        />
+        <Divider />
         <Main open={open}>
           {selectedUser ? (
             <>
@@ -68,39 +67,42 @@ export default function ChatSection() {
                 This is the chat area for {selectedUser.name}.
               </Typography>
               <Typography sx={{ marginBottom: 2 }}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non
-                enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus
-                imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus.
-                Convallis convallis tellus id interdum velit laoreet id donec ultrices.
-                Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-                adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra
-                nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum
-                leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis
-                feugiat vivamus at augue. At augue eget arcu dictum varius duis at
-                consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Rhoncus dolor purus non enim praesent elementum facilisis leo
+                vel. Risus at ultrices mi tempus imperdiet. Semper risus in
+                hendrerit gravida rutrum quisque non tellus. Convallis convallis
+                tellus id interdum velit laoreet id donec ultrices. Odio morbi
+                quis commodo odio aenean sed adipiscing. Amet nisl suscipit
+                adipiscing bibendum est ultricies integer quis. Cursus euismod
+                quis viverra nibh cras. Metus vulputate eu scelerisque felis
+                imperdiet proin fermentum leo. Mauris commodo quis imperdiet
+                massa tincidunt. Cras tincidunt lobortis feugiat vivamus at
+                augue. At augue eget arcu dictum varius duis at consectetur
+                lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa
                 sapien faucibus et molestie ac.
               </Typography>
               <Typography sx={{ marginBottom: 2 }}>
-                Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper
-                eget nulla facilisi etiam dignissim diam. Pulvinar elementum integer enim
-                neque volutpat ac tincidunt. Ornare suspendisse sed nisi lacus sed viverra
-                tellus. Purus sit amet volutpat consequat mauris. Elementum eu facilisis
-                sed odio morbi. Euismod lacinia at quis risus sed vulputate odio. Morbi
-                tincidunt ornare massa eget egestas purus viverra accumsan in. In hendrerit
-                gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem
-                et tortor. Habitant morbi tristique senectus et. Adipiscing elit duis
-                tristique sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
-                eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
-                posuere sollicitudin aliquam ultrices sagittis orci a.
+                Consequat mauris nunc congue nisi vitae suscipit. Fringilla est
+                ullamcorper eget nulla facilisi etiam dignissim diam. Pulvinar
+                elementum integer enim neque volutpat ac tincidunt. Ornare
+                suspendisse sed nisi lacus sed viverra tellus. Purus sit amet
+                volutpat consequat mauris. Elementum eu facilisis sed odio
+                morbi. Euismod lacinia at quis risus sed vulputate odio. Morbi
+                tincidunt ornare massa eget egestas purus viverra accumsan in.
+                In hendrerit gravida rutrum quisque non tellus orci ac.
+                Pellentesque nec nam aliquam sem et tortor. Habitant morbi
+                tristique senectus et. Adipiscing elit duis tristique
+                sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
+                eleifend. Commodo viverra maecenas accumsan lacus vel facilisis.
+                Nulla posuere sollicitudin aliquam ultrices sagittis orci a.
               </Typography>
-
             </>
           ) : (
             <Typography>Select a user to start chatting.</Typography>
           )}
         </Main>
-      <DrawerComponent open={open} handleDrawerClose={handleDrawerClose} />
+        <DrawerComponent open={open} handleDrawerClose={handleDrawerClose} />
       </Box>
       {/* Right Drawer - togglable */}
     </Box>
